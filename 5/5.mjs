388312b87aaa719f4466a87afca60d9d5e1f7359
Denis <denis.lovic@sogeti.se>
2022-12-06 19:15:01 +0100

    5

---
+++ 5/5.mjs
@@ -0,0 +1,70 @@
+import { getInput } from "../utils.mjs";
+const input = getInput(5, "input");
+
+const [sketch, instructionsSection] = input.split("\r\n\r\n");
+const instructions = instructionsSection.split("\r\n");
+
+const getStacks = (indexes) => {
+  let stackLines = sketch.split("\r\n");
+  let levels = stackLines.slice(0, stackLines.length - 1).reverse();
+
+  return indexes.reduce((allStacks, index) => {
+    const stack = [];
+    levels.every((l) => {
+      if (l[index] !== " ") {
+        stack.push(l[index]);
+        return true;
+      }
+
+      return false;
+    });
+
+    return [...allStacks, stack];
+  }, []);
+};
+
+const getStackIndexes = (lines) => {
+  let indexes = [];
+
+  lines.every((line, lineNumber) => {
+    if (line.indexOf("1") > -1) {
+      indexes = [...lines[lineNumber]]
+        .filter((x) => x !== " ")
+        .map((x) => lines[lineNumber].indexOf(x));
+
+      return false;
+    }
+
+    return true;
+  });
+
+  return indexes;
+};
+
+const executeInstructions = (stacks, instruction) => {
+  // fucking js
+  const [move, from, to] = instruction
+    .replace("move", "")
+    .replace("from", "")
+    .replace("to", "")
+    .split("  ")
+    .map((x) => parseInt(x));
+
+  for (let i = 0; i < move; i++) {
+    let crate = stacks[from - 1].pop();
+    stacks[to - 1].push(crate);
+  }
+};
+
+let stackIndexes = getStackIndexes(sketch.split("\r\n"));
+let stacks = getStacks(stackIndexes);
+
+console.log(sketch);
+console.log(stacks);
+instructions.forEach((instruction) => executeInstructions(stacks, instruction));
+
+const message = stacks.reduce((message, stack) => {
+  return (message += [...stack].reverse()[0]);
+}, "");
+
+console.log(message);
